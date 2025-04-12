3e6108a4f92d50299277901e4769b3efa380d28c
Liang-Shih Lin <liangshihlin@gmail.com>
2025-04-12 17:49:50 +0800

    ci(fix): Add sentry vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { plugin as mdPlugin, Mode } from "vite-plugin-markdown";
@@ -18,6 +19,13 @@ export default defineConfig(() => ({
         // console.log(rendered);
         return rendered;
       },
+    }),
+    sentryVitePlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+
+      // Auth tokens can be obtained from https://sentry.io/orgredirect/organizations/:orgslug/settings/auth-tokens/
+      authToken: process.env.SENTRY_AUTH_TOKEN,
     }),
   ].filter(Boolean),
   resolve: {
