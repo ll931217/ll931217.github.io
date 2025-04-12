1f4348397ada3b37ef040d48dcdb48cac95cc010
Liang-Shih Lin <liangshihlin@gmail.com>
2025-04-12 17:26:29 +0800

    feat: Added sentry

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import { createRoot } from "react-dom/client";
+import * as Sentry from "@sentry/react";
 import App from "./App.tsx";
 import "./index.css";
 import "highlight.js/styles/tokyo-night-dark.min.css";
+
+Sentry.init({
+  dsn: "https://70aa6d6bd66bb6adefddba3d717d17f6@sentry.baoge.dev/3",
+});
 
 createRoot(document.getElementById("root")!).render(<App />);
